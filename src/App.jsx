4f920031bd7ea1114f4253af64f09da79cd719de
Charlie025x <charliealonso2001@gmail.com>
2023-11-09 18:37:54 -0600

    designed and added placeholder text on credit cards

--- src/App.jsx
+++ src/App.jsx
@@ -13,20 +13,29 @@ export default function App() {
     <div className="min-h-screen bg-gray-400">
       <div className="h-60 w-full bg-mobile-hero-image bg-cover">
         <div className="relative mx-auto h-full max-w-md">
-          {/* <div className="absolute right-5 top-10 h-3/5 w-3/4 max-w-[300px] bg-card-back bg-contain bg-right bg-no-repeat">
-            <p className="absolute right-8 top-[58px] text-white">000</p>
-          </div> */}
-          <div className="absolute right-5 top-10 w-3/4 max-w-[300px] border-4 ">
+          {/* card back */}
+          <div className="absolute right-5 top-10 w-3/4 max-w-[300px]">
             <img src={"./src/images/bg-card-back.png"} alt="" className="" />
-            <p className="absolute right-[15%] top-[40%] text-white">000</p>
+            <p className="absolute right-[15%] top-[44%] text-xs text-white">
+              000
+            </p>
           </div>
-          {/* <div className="absolute bottom-[-2.5rem] left-5 h-3/5 w-3/4 max-w-[300px] border-4 bg-card-front bg-contain bg-no-repeat">
-            <p className="absolute left-8 top-[58px] text-white">
-            <p className=" text-center text-white">0000 0000 0000 0000</p>
-          </div> */}
-          <div className="absolute left-5 top-32 w-3/4 max-w-[300px] border-4 ">
-            <img src={"./src/images/bg-card-front.png"} alt="" className="" />
-            <p className="top-0 h-0 px-4 text-white">0000 0000 0000 0000</p>
+          {/* card front */}
+          <div className="absolute left-5 top-32 w-3/4 max-w-[300px]">
+            <img src={"./src/images/bg-card-front.png"} alt="" />
+            <div className="absolute top-4">
+              <div className="flex items-center">
+                <div className=" ml-4 h-6 w-6 rounded-full bg-white"></div>
+                <div className="ml-2  h-4 w-4 rounded-full border"></div>
+              </div>
+            </div>
+            <div className="absolute bottom-4 w-full px-4 text-justify text-white">
+              <p className="pb-3 text-justify">0000 0000 0000 0000</p>
+              <div className="flex justify-between text-xs">
+                <p className="">JANE APPLESEED</p>
+                <p className="">00/00</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
