31ebcbb6e3612d04fc23ea6ff0ea29bce187bc31
Charlie025x <charliealonso2001@gmail.com>
2023-11-12 11:13:35 -0600

    form complete message on form successful submit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { data } from "autoprefixer";
 import { useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
+import { useEffect } from "react";
 
 export default function App() {
-  const { register, handleSubmit, watch, control, formState } = useForm();
-  const { errors } = formState;
+  const { register, handleSubmit, watch, control, formState, setValue } =
+    useForm();
+  const { errors, isDirty, isValid, isSubmitSuccessful } = formState;
 
   const onSubmit = (data) => console.log(data);
 
@@ -15,7 +17,7 @@ export default function App() {
   const yy = watch("dateY");
 
   return (
-    <div className="min-h-screen bg-gray-400">
+    <div className="min-h-screen">
       {/* card container */}
       <div className="h-60 w-full bg-mobile-hero-image bg-cover">
         <div className="relative mx-auto h-full max-w-md">
@@ -50,145 +52,192 @@ export default function App() {
         </div>
       </div>
       {/* form container */}
-      <div className="bg-[hsl(270, 3%, 87%)] mx-auto h-[70vh] w-full max-w-2xl px-4 pt-20">
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          noValidate
-          className="grid grid-cols-4 gap-4"
-        >
-          <div className="col-span-4">
-            <label htmlFor="name" className="font-bold uppercase	text-[#21092f]">
-              cardholder name
-            </label>
-            <input
-              type="text"
-              id="name"
-              maxLength={30}
-              {...register("name", { required: "can't be blank" })}
-              className="my-4 block w-full rounded-lg border-2	border-[#8e8593] bg-transparent p-2 placeholder:text-[#8e8593]"
-              placeholder="e.g. Jane Appleseed"
-            />
-            <p className="translate-y-[-.75rem] text-red-600">
-              {errors.name?.message}
-            </p>
-          </div>
-
-          <div className="col-span-4">
-            <label
-              htmlFor="cardNumber"
-              className="font-bold uppercase text-[#21092f]"
-            >
-              card number
-            </label>
-            {/* check for non numbers and add error message */}
-            <input
-              type="text"
-              id="cardNumber"
-              maxLength={16}
-              {...register("cardNumber", {
-                required: "can't be blank",
-                pattern: {
-                  value: /[0-9]/,
-                  message: "Wrong format, numbers only",
-                },
-                maxLength: 16,
-              })}
-              className="my-4 block w-full rounded-lg border-2	border-[#8e8593] bg-transparent p-2 placeholder:text-[#8e8593]"
-              placeholder="e.g. 1234 5678 9123 0000"
-            />
-            <p className="col-span-4 translate-y-[-.75rem] text-red-600">
-              {errors.cardNumber?.message}
-            </p>
-          </div>
-
-          <div className="col-span-2">
-            <label
-              htmlFor="dateM"
-              className="font-bold	uppercase text-[#21092f]"
-            >
-              exp. date (mm/yy)
-            </label>
-            {/* TODO: concatenate the dateY into the date state */}
-            <div className="flex gap-4">
+      <div className="mx-auto h-[70vh] w-full max-w-2xl px-4 pt-20">
+        {!isSubmitSuccessful && (
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            noValidate
+            className="grid grid-cols-4 gap-4"
+          >
+            <div className="col-span-4">
+              <label
+                htmlFor="name"
+                className="font-bold uppercase	text-[#21092f]"
+              >
+                cardholder name
+              </label>
               <input
                 type="text"
-                id="date"
-                maxLength={2}
-                {...register("date", {
-                  required: "can't be blank",
-                  pattern: {
-                    value: /[0-9]/,
-                    message: "Wrong format, numbers only",
-                  },
-                  minLength: {
-                    value: 2,
-                    message: "Enter 2 digit value",
-                  },
-                })}
-                className="my-4 w-full rounded-lg	border-2 border-[#8e8593] bg-transparent p-2 placeholder:text-[#8e8593]"
-                placeholder="MM"
+                id="name"
+                maxLength={30}
+                {...register("name", { required: "can't be blank" })}
+                className="my-4 block w-full rounded-lg border-2	border-[#8e8593] bg-transparent p-2 placeholder:text-[#8e8593]"
+                placeholder="e.g. Jane Appleseed"
               />
-              <input
-                type="text"
-                id="dateY"
-                maxLength={2}
-                {...register("dateY", {
-                  required: "can't be blank",
-                  pattern: {
-                    value: /[0-9]/,
-                    message: "Wrong format, numbers only",
-                  },
-                  minLength: {
-                    value: 2,
-                    message: "Enter 2 digit value",
-                  },
-                })}
-                className="my-4 w-full rounded-lg	border-2 border-[#8e8593] bg-transparent p-2 placeholder:text-[#8e8593]"
-                placeholder="YY"
-              />
+              <p className="translate-y-[-.75rem] text-red-600">
+                {errors.name?.message}
+              </p>
             </div>
-            <p className="translate-y-[-.75rem] text-red-600">
-              {errors.date?.message || errors.dateY?.message}
-            </p>
-          </div>
 
-          <div className="col-span-2">
-            <label htmlFor="cvc" className="font-bold uppercase	text-[#21092f]">
-              cvc
-            </label>
-            <input
-              type="text"
-              id="cvc"
-              maxLength={3}
-              {...register("cvc", {
-                required: "can't be blank",
-                pattern: {
-                  value: /[0-9]/,
-                  message: "Wrong format, numbers only",
-                },
-                minLength: {
-                  value: 2,
-                  message: "Enter 3 digit value",
-                },
-              })}
-              className="my-4 block w-full rounded-lg border-2	border-[#8e8593] bg-transparent p-2 placeholder:text-[#8e8593]"
-              placeholder="e.g. 123"
-            />
-            <p className="translate-y-[-.75rem] text-red-600">
-              {errors.cvc?.message}
-            </p>
-          </div>
+            <div className="col-span-4">
+              <label
+                htmlFor="cardNumber"
+                className="font-bold uppercase text-[#21092f]"
+              >
+                card number
+              </label>
+              {/* check for non numbers and add error message */}
+              <input
+                type="text"
+                id="cardNumber"
+                maxLength={19}
+                {...register("cardNumber", {
+                  required: "can't be blank",
+                  pattern: {
+                    value: /[0-9]/,
+                    message: "Wrong format, numbers only",
+                  },
+                  // maxLength: 16,
+                })}
+                className="my-4 block w-full rounded-lg border-2	border-[#8e8593] bg-transparent p-2 placeholder:text-[#8e8593]"
+                placeholder="e.g. 1234 5678 9123 0000"
+                onKeyDown={(e) => {
+                  const backspacePressed = e.key !== "Backspace";
+                  const currentValue = e.currentTarget.value;
+                  if (backspacePressed) {
+                    const lengthNoSpaces = currentValue.replace(
+                      / /g,
+                      "",
+                    ).length;
+                    if (
+                      lengthNoSpaces !== 0 &&
+                      lengthNoSpaces % 4 === 0 &&
+                      currentValue.length < 17
+                    ) {
+                      setValue("cardNumber", currentValue + " ");
+                    }
+                  } else {
+                    if (currentValue[currentValue.length - 2] === " ") {
+                      setValue(
+                        "cardNumber",
+                        currentValue.substr(0, currentValue.length - 1),
+                      );
+                    }
+                  }
+                }}
+              />
+              <p className="col-span-4 translate-y-[-.75rem] text-red-600">
+                {errors.cardNumber?.message}
+              </p>
+            </div>
 
-          <button className="col-span-4  w-full rounded-lg bg-[#21092f] py-2 text-white">
-            Confirm
-          </button>
-        </form>
+            <div className="col-span-2">
+              <label
+                htmlFor="dateM"
+                className="font-bold	uppercase text-[#21092f]"
+              >
+                exp. date (mm/yy)
+              </label>
+              {/* TODO: concatenate the dateY into the date state */}
+              <div className="flex gap-4">
+                <input
+                  type="text"
+                  id="date"
+                  maxLength={2}
+                  {...register("date", {
+                    required: "can't be blank",
+                    pattern: {
+                      value: /[0-9]/,
+                      message: "Wrong format, numbers only",
+                    },
+                    minLength: {
+                      value: 2,
+                      message: "Enter 2 digit value",
+                    },
+                  })}
+                  className="my-4 w-full rounded-lg	border-2 border-[#8e8593] bg-transparent p-2 placeholder:text-[#8e8593]"
+                  placeholder="MM"
+                />
+                <input
+                  type="text"
+                  id="dateY"
+                  maxLength={2}
+                  {...register("dateY", {
+                    required: "can't be blank",
+                    pattern: {
+                      value: /[0-9]/,
+                      message: "Wrong format, numbers only",
+                    },
+                    minLength: {
+                      value: 2,
+                      message: "Enter 2 digit value",
+                    },
+                  })}
+                  className="my-4 w-full rounded-lg	border-2 border-[#8e8593] bg-transparent p-2 placeholder:text-[#8e8593]"
+                  placeholder="YY"
+                />
+              </div>
+              <p className="translate-y-[-.75rem] text-red-600">
+                {errors.date?.message || errors.dateY?.message}
+              </p>
+            </div>
+
+            <div className="col-span-2">
+              <label
+                htmlFor="cvc"
+                className="font-bold uppercase	text-[#21092f]"
+              >
+                cvc
+              </label>
+              <input
+                type="text"
+                id="cvc"
+                maxLength={3}
+                {...register("cvc", {
+                  required: "can't be blank",
+                  pattern: {
+                    value: /[0-9]/,
+                    message: "Wrong format, numbers only",
+                  },
+                  minLength: {
+                    value: 2,
+                    message: "Enter 3 digit value",
+                  },
+                })}
+                className="my-4 block w-full rounded-lg border-2	border-[#8e8593] bg-transparent p-2 placeholder:text-[#8e8593]"
+                placeholder="e.g. 123"
+              />
+              <p className="translate-y-[-.75rem] text-red-600">
+                {errors.cvc?.message}
+              </p>
+            </div>
+
+            <button
+              // disabled={!isDirty || !isValid}
+              className="col-span-4  w-full rounded-lg bg-[#21092f] py-2 text-white"
+            >
+              Confirm
+            </button>
+          </form>
+        )}
         <DevTool control={control} />
 
-        {/* <input type="submit" className=" block bg-red-500 px-2" /> */}
-        {/* <button className="w-full  rounded-lg bg-[#21092f] py-2 text-white">
-          Confirm
-        </button> */}
+        {isSubmitSuccessful && (
+          <div className=" flex flex-col items-center gap-4 text-center">
+            <img
+              className=" my-4 w-24"
+              src="./src/images/icon-complete.svg"
+              alt=""
+            />
+            <h1 className="text-3xl uppercase tracking-wider text-[#21092f]">
+              thank you!
+            </h1>
+            <p className="mb-4 text-[#8e8593]">We've added your card details</p>
+            <button className="col-span-4  w-full max-w-lg rounded-lg bg-[#21092f] py-2 text-white">
+              Continue
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
